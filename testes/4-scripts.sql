3b49ac2cd9bdd99296e05f2619c90cd4bdb28788
victordev018 <joawrt8520@gmail.com>
2025-07-05 10:33:47 -0300

    update: atualiza as coisas que a sammya num fez

--- testes/4-scripts.sql
+++ testes/4-scripts.sql
@@ -1,23 +1,26 @@
 -- Criar cliente com procedure genérica
-CALL inserir_dados('cliente', 'nome, email, telefone', '''Ricardo'', ''ricardo@gmail.com'', ''31955554644''')
-CALL inserir_dados('cliente', 'nome, email, telefone', '''Igor'', ''igor@gmail.com'', ''31955554622''')
+CALL inserir_dados('cliente', 'nome, email, telefone', '''Ricardo'', ''ricardo@gmail.com'', ''31955554644''');
+CALL inserir_dados('cliente', 'nome, email, telefone', '''Igor'', ''igor@gmail.com'', ''31955554622''');
 
-select * from cliente
+-- Buscando clientes ativos por meio de um view
+select * from vw_clientes_ativos;
 
 -- Atualizar nome do cliente
 CALL atualizar_dados('cliente', 'nome', '''Ricardo Ramos''', 'nome = ''Ricardo''');
 
 -- Atualizar email do cliente
-CALL atualizar_dados('cliente', 'email', '''igornasc@gmail.com''', 'cod_cliente = 5');
+CALL atualizar_dados('cliente', 'email', '''igornasc@gmail.com''', 'nome = ''Igor'' ');
 
 -- Deve dá erro no trigger para verificação de email duplicado
-CALL atualizar_dados('cliente', 'email', '''marcelino@email.com''', 'nome = ''Ricardo Ramos''');
+CALL atualizar_dados('cliente', 'email', '''marcelino@email.com''', 'nome = ''Ricardo Ramos'' ');
 
 -- Deve dá erro no trigger para verificação de quantidade de dígitos do telefone
-CALL inserir_dados('cliente', 'nome, email, telefone', '''Aline'', ''aline@gmail.com'', ''319554644''')
+CALL inserir_dados('cliente', 'nome, email, telefone', '''Aline'', ''aline@gmail.com'', ''319554644'' ');
 
 -- Deletar lógico para cliente
-call deletar_logico('cliente', 'nome = ''Ricardo Ramos''')
-call deletar_logico('cliente', 'cod_cliente = 5')
+call deletar_logico('cliente', 'nome = ''Ricardo Ramos''');
+call deletar_logico('cliente', 'nome = ''Igor'' ');
 
-select * from cliente
+-- Buscar clientes desativados por meio de uma view
+select * from vw_clientes_inativos;
+
